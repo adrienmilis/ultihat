0549c4324368ce009b107a11d9864ea7bf64f4f3
Adrien Milis <adrien.milis@brussels.msf.org>
2025-09-24 17:18:24 +0200

    Scaffold module

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "ultihat",
+    "summary": "Ultihat",
+    "author": "miad",
+    "category": "Uncategorized",
+    "version": "19.0.1.0.0",
+    "depends": ["contacts"],
+    "data": [],
+    "assets": {},
+}
+
