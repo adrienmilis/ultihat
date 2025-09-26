2d325a331ed55355070b681d9538439c5fa283d7
Adrien Milis <adrien.milis@brussels.msf.org>
2025-09-26 17:25:30 +0200

    wip

--- __manifest__.py
+++ __manifest__.py
@@ -1,11 +1,19 @@
 # -*- coding: utf-8 -*-
 {
     "name": "ultihat",
-    "summary": "Ultihat",
+    "summary": "Ultihat SaaS",
     "author": "miad",
     "category": "Uncategorized",
     "version": "19.0.1.0.0",
     "depends": ["contacts"],
-    "data": [],
+    "data": [
+        # models
+        "models/x_club.xml",
+        "models/res_partner.xml",
+        # views
+        "views/x_club_views.xml",
+        # security
+        "security/ir.model.access.csv",
+    ],
     "assets": {},
 }
