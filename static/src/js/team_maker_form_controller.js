fb8be7d550a0149490c4817e3a5d7b449b14d353
Adrien Milis <adrien.milis@brussels.msf.org>
2026-03-02 19:16:31 +0100

    todo: js

--- static/src/js/team_maker_form_controller.js
+++ static/src/js/team_maker_form_controller.js
@@ -19,37 +19,37 @@ export const TeamMakerFormView = {
 registry.category("views").add("team_maker", TeamMakerFormView);
 
 // Helper: variance calculation
-function variance(arr) {
-  const mean = arr.reduce((a, b) => a + b, 0) / arr.length;
-  return arr.reduce((sum, val) => sum + (val - mean) ** 2, 0) / arr.length;
-}
+// function variance(arr) {
+//   const mean = arr.reduce((a, b) => a + b, 0) / arr.length;
+//   return arr.reduce((sum, val) => sum + (val - mean) ** 2, 0) / arr.length;
+// }
 
-function safeVariance(arr) {
-  const unique = new Set(arr);
-  return unique.size > 1 ? variance(arr) : 0;
-}
+// function safeVariance(arr) {
+//   const unique = new Set(arr);
+//   return unique.size > 1 ? variance(arr) : 0;
+// }
 
-// Evaluate how balanced the teams are
-function evaluateTeams(teams) {
-  const avgLevels = teams.map(team =>
-    team.reduce((sum, p) => sum + p.level, 0) / team.length
-  );
+// // Evaluate how balanced the teams are
+// function evaluateTeams(teams) {
+//   const avgLevels = teams.map(team =>
+//     team.reduce((sum, p) => sum + p.level, 0) / team.length
+//   );
 
-  const handlerCounts = teams.map(team =>
-    team.filter(p => p.role === 'H').length
-  );
+//   const handlerCounts = teams.map(team =>
+//     team.filter(p => p.role === 'H').length
+//   );
 
-  const femaleCounts = teams.map(team =>
-    team.filter(p => p.gender === 'F').length
-  );
+//   const femaleCounts = teams.map(team =>
+//     team.filter(p => p.gender === 'F').length
+//   );
 
-  const score =
-    safeVariance(avgLevels) +
-    0.5 * safeVariance(handlerCounts) +
-    0.5 * safeVariance(femaleCounts);
+//   const score =
+//     safeVariance(avgLevels) +
+//     0.5 * safeVariance(handlerCounts) +
+//     0.5 * safeVariance(femaleCounts);
 
-  return score;
-}
+//   return score;
+// }
 
 // Main function to create balanced teams
 // function makeTeams(players, nTeams, playersPerTeam, maxIterations=10000) {
